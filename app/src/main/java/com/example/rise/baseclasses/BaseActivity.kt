c39cd090cc037a44170a07dfe2d3ec6cc419161d
amonkhepri <sirthesurfer@gmail.com>
2025-09-30 10:41:38 +0200

    Defer base view model lookup to avoid launch NPE

--- app/src/main/java/com/example/rise/baseclasses/BaseActivity.kt
+++ app/src/main/java/com/example/rise/baseclasses/BaseActivity.kt
@@ -2,7 +2,7 @@ package com.example.rise.baseclasses
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModel
+import kotlin.LazyThreadSafetyMode
 import kotlin.reflect.KClass
 import org.koin.androidx.viewmodel.ext.android.viewModelForClass
 import org.koin.core.parameter.ParametersDefinition
@@ -14,18 +14,16 @@ abstract class BaseActivity<VM : BaseViewModel> : AppCompatActivity() {
     protected open val viewModelParameters: ParametersDefinition? = null
     protected open val viewModelQualifier: Qualifier? = null
 
-    private val viewModelDelegate: Lazy<out ViewModel> = viewModelForClass(
-        clazz = viewModelClass,
-        qualifier = viewModelQualifier,
-        parameters = viewModelParameters,
-    )
-
-    @Suppress("UNCHECKED_CAST")
-    protected val viewModel: VM
-        get() = viewModelDelegate.value as VM
+    protected val viewModel: VM by lazy(LazyThreadSafetyMode.NONE) {
+        viewModelForClass(
+            clazz = viewModelClass,
+            qualifier = viewModelQualifier,
+            parameters = viewModelParameters,
+        ).value
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewModelDelegate.value
+        viewModel
     }
 }
